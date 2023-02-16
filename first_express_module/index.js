7c729f899c432abdfe886c31218eda74a6994bed
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-17 00:53:50 +0530

    Initial Commit

--- first_express_module/index.js
+++ first_express_module/index.js
@@ -1,14 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("This is our Home Page");
+  // res.send("This is our Home Page");
+
+  res.send(`
+  <input type = "text" placeholder = "name">
+  <input type = "submit">
+  <a href="/about">Go to About Page</a>
+  `);
 });
 app.get("/about", (req, res) => {
-  res.send("This is our About Page");
+  res.send(`
+  This is our About Page
+  <a href="/">Go to Home Page</a>
+  `);
 });
 app.get("/contact", (req, res) => {
-  res.send("This is our Contact Page");
+  // res.send("This is our Contact Page");
+
+  res.send({
+    name: "Priyanshu",
+    email: "priyanshujha131@gmail.com",
+  });
 });
 app.get("/service", (req, res) => {
   res.send("This is our Service Page");
