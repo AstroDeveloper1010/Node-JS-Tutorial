ea39649b7c48281a95980990b66893069d84092c
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-17 01:35:22 +0530

    New Commit

--- sending_html_page_to_app/server.js
+++ sending_html_page_to_app/server.js
@@ -1,10 +1,16 @@
-const path = require('path')
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
-const pathFile = path.join(__dirname, 'public');
+const pathFile = path.join(__dirname, "public");
 console.log(pathFile);
 
-app.use(express.static(pathFile));
+// app.use(express.static(pathFile)); // used to send static files
+app.get("", (req, res) => {
+  res.sendFile(`${pathFile}/index.html`); // using sendFile we can hide the extension .html
+});
+app.get("/contact", (req, res) => {
+  res.sendFile(`${pathFile}/contact.html`);
+});
 
 app.listen(4500);
