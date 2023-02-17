c85b1ffb7322479c21b01592bcbd851a5378572a
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-17 14:06:01 +0530

    created common directory inside views directory

--- dynamic_page_with_EJS/server.js
+++ dynamic_page_with_EJS/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express');
 
 const app = express();
-const pathfile = path.join(__dirname, 'public'); 
+const pathfile = path.join(__dirname, 'public');
 
 app.set('view engine', 'ejs');
 
@@ -18,11 +18,14 @@ app.get('/about', (req, res) => {
     const user = {
         name: 'Priyanshu Jha',
         email: 'priyanshujha131@gmail.com',
-        age: '20',
-        city: 'New Delhi',
+        profession: 'student',
         skills: ['javascript', 'c++', 'python', 'c', 'ui/ux designing', 'graphic designing', 'node js', 'express js']
     }
-    res.render('about', {user});
+    res.render('about', { user });
+})
+
+app.get('/login', (req, res) => {
+    res.render('login');
 })
 
 app.listen(4000);
