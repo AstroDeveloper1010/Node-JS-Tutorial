08bca554719d6bad9df083ee34af6947c9bcb252
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-17 22:34:31 +0530

    updated

--- calculator_using_body_parser/server.js
+++ calculator_using_body_parser/server.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
+const { dirname } = require('path');
 
 const app = express();
 
+const filepath = path.join(__dirname, 'public')
+
 app.get('/', (req, res) => {
     res.send("This is Home Page");
+})
+app.get('/calculator', (req, res) => {
+    res.sendFile(`${filepath}/calculator.html`);
 })
 
 app.listen(4000);
