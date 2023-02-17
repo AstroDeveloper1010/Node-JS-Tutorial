9a3bd92601af057e810259eecd980624d930f4e3
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-17 23:19:18 +0530

    updated

--- calculator_using_body_parser/server.js
+++ calculator_using_body_parser/server.js
@@ -4,17 +4,23 @@ const express = require('express');
 const { dirname } = require('path');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const filepath = path.join(__dirname, 'public')
 
 app.get('/', (req, res) => {
-    res.send("This is Home Page");
+    res.sendFile(`${filepath}/index.html`);
 })
 app.get('/calculator', (req, res) => {
     res.sendFile(`${filepath}/calculator.html`);
 })
-app.post('/calculator', (req, res) => {
-    res.send('Input Fetched');
+app.post('/', (req, res) => {
+    console.log(req.body);
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let sum = num1 + num2;
+    res.send("The sum is " + sum);
+    res.end();
 })
 
 app.listen(4000);
