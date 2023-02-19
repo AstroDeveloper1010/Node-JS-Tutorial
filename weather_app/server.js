941547a1d7dec5ffa94d9c5172ce65d1017759a4
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-19 19:56:44 +0530

    updated serer.js

--- weather_app/server.js
+++ weather_app/server.js
@@ -5,18 +5,32 @@ const app = express();
 
 app.get('/', (req, res) => {
 
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=1cdc5bf6abf70ae6ca69a708a09393eb';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=1cdc5bf6abf70ae6ca69a708a09393eb&units=metric';
     https.get(url, (resp) => {
         console.log(resp.statusCode);
 
         resp.on('data', (data) => {
             const weatherData = JSON.parse(data);
-            console.log(weatherData);
+
+            const temp = weatherData.main.temp;
+            const desc = weatherData.weather[0].description;
+            const pressure = weatherData.main.pressure;
+            const humidity = weatherData.main.humidity;
+
+            // res.write(temp);
+            // res.write(desc);
+            // res.write(pressure);
+            // res.write(humidity);
+            res.write('<p>Weather is ' + desc + '</p>')
+            res.write('<p>temperature is' + temp + '</p>');
+            res.write('<p>humidity is ' + humidity + '</p>');
+            res.write('<p>pressure is ' + pressure + '</p>');
+            res.send();
         })
     })
 
     // res.write("This is home page");
-    res.send('Server is running');
+
     // res.end();
 })
 
