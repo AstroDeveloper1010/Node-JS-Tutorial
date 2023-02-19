381ab3bb26f8ccc08c355f3946a3bbcb2d22c15b
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-20 00:05:56 +0530

    updated server.js

--- weather_app/server.js
+++ weather_app/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const filepath = path.join(__dirname, 'public');
 
@@ -18,11 +18,16 @@ app.listen(4000, () => {
 });
 
 app.post('/', (req, res) => {
-    
-})
+    console.log(req.body.latitude);
+    console.log(req.body.longitude);
 
-/*
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=1cdc5bf6abf70ae6ca69a708a09393eb&units=metric';
+    // const lati = 44.34;
+    const lati = req.body.latitude;
+    // const longi = 10.99;
+    const longi = req.body.longitude;
+    const units = 'metric'
+    const appid = '1cdc5bf6abf70ae6ca69a708a09393eb'
+    const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lati + '&lon=' + longi + '&appid=' + appid + '&units=' + units;
     https.get(url, (resp) => {
         console.log(resp.statusCode);
 
@@ -35,10 +40,11 @@ app.post('/', (req, res) => {
             const humidity = weatherData.main.humidity;
 
             res.write('<p>Weather is ' + desc + '</p>')
-            res.write('<p>temperature is' + temp + '</p>');
+            res.write('<p>temperature is ' + temp + '</p>');
             res.write('<p>humidity is ' + humidity + '</p>');
             res.write('<p>pressure is ' + pressure + '</p>');
-            res.send();
+
+            res.end();
         })
     })
-*/
+});
