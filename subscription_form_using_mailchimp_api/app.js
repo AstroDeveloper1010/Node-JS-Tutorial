32fbcfa9746b10a60d6e5af59c4322a52ce444bd
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-21 17:48:22 +0530

    updated

--- subscription_form_using_mailchimp_api/app.js
+++ subscription_form_using_mailchimp_api/app.js
@@ -3,6 +3,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
+const pathfile = path.join(__dirname, 'public');
+
+app.get('/', (req, res) => {
+    res.sendFile(`${pathfile}/index.html`);
+})
 
 app.listen(4000, () => {
     console.log("server is running at port 4000");
