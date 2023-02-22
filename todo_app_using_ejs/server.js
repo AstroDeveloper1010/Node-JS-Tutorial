894a94b555c53419284e6b3c3a6b566b53d6e4b2
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-23 00:45:16 +0530

    added list.ejs

--- todo_app_using_ejs/server.js
+++ todo_app_using_ejs/server.js
@@ -8,8 +8,41 @@ const app = express();
 
 const filepath = path.join(__dirname, 'public');
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.sendFile(`${filepath}/index.html`)
+    // res.sendFile(`${filepath}/index.html`)
+
+    const today = new Date();
+    const currentDay = today.getDay();
+    let day = '';
+
+    switch (currentDay) {
+        case 0:
+            day = 'Sunday'
+            break;
+        case 1:
+            day = 'Monday'
+            break;
+        case 2:
+            day = 'Tuesday'
+            break;
+        case 3:
+            day = 'Wednesday'
+            break;
+        case 4:
+            day = 'Thursday'
+            break;
+        case 5:
+            day = 'Friday'
+            break;
+        case 6:
+            day = 'Saturday'
+            break;
+        default:
+    }
+
+    res.render('list', { $day: day });
 })
 
 const port = 4000;
