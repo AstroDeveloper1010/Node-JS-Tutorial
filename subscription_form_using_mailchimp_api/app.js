ccd66b89021830bc6ff105c4615af1d39045a6ff
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-22 14:16:27 +0530

    updated

--- subscription_form_using_mailchimp_api/app.js
+++ subscription_form_using_mailchimp_api/app.js
@@ -5,14 +5,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const pathfile = path.join(__dirname, 'public');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
     const fname = req.body.firstname;
     const lname = req.body.lastname;
     const email = req.body.email;
+    const password = req.body.password;
 
     console.log(fname + " " + lname + " " + email);
+    console.log(password);
 
     res.send('You have successfully subscribed our service.')
 })
