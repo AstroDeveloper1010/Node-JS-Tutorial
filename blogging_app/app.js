7ee998d6fe7c4a2c3c7477b08e3b7e56e9e11775
Priyanshu Jha <priyanshujha131@gmail.com>
2023-02-23 20:00:01 +0530

    updated

--- blogging_app/app.js
+++ blogging_app/app.js
@@ -14,8 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('home', { home: homeStartingContent, about: aboutContent, content: contactContent });
-})
+  res.render('home', { home: homeStartingContent});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {about: aboutContent});
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {contact: contactContent});
+});
 
 const port = 4000;
 app.listen(port, () => {
